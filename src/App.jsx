0a05558aa5b550564fe2ff74a9818af1794eac1e
Thomas Garcia <tjg9265@yahoo.com>
2026-03-03 20:23:11 -0700

    Todo list working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [newTodo, setNewTodo] = useState("")
+  const[todos, setTodos] = useState([])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (newTodo ==="") return
+
+    setTodos(current => [
+      ...current,
+      {id: crypto.randomUUID(), title: newTodo, completed: false}
+    ])
+
+    setNewTodo("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(current =>
+      current.map(todo => {
+        if (todo.id === id) {
+          return{...todo, completed}
+        }
+        return todo
+      })
+    )
+  }
+
+  function deleteTodo(id) {
+    setTodos(current => current.filter(todo => todo.id !== id))
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>TodoList</h1>
+     <form onSubmit={handleSubmit}>
+      <input
+        value={newTodo}
+        onChange={e => setNewTodo(e.target.value)}
+        type="text"
+      />
+      <button>Add</button>
+     </form>
+
+     <ul>
+      {todos.map(todo =>(
+        <li key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={e =>
+                toggleTodo(todo.id, e.target.checked)  
+              }
+            />
+            {todo.title}  
+          </label>
+            <button onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+
+        </li>
+      ))}
+     </ul>
+    </> 
   )
 }
-
-export default App
